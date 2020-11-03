1a72ef0d769ef7b19dd961718f03467a6cf8c304
rogerrafael7 <rogerrafael7@gmail.com>
2020-11-03 06:09:28 -0300

    ...

--- middleware/pipedrive-authorization.js
+++ middleware/pipedrive-authorization.js
@@ -4,8 +4,9 @@ const oAuthManager = lib.OAuthManager
 lib.Configuration.apiToken = process.env.PIPEDRIVE_API_TOKEN
 lib.Configuration.oAuthClientId = process.env.PIPEDRIVE_CLIENT_ID // OAuth 2 Client ID
 lib.Configuration.oAuthClientSecret = process.env.PIPEDRIVE_CLIENT_SECRET // OAuth 2 Client Secret
-lib.Configuration.oAuthRedirectUri = '/auth/pipedrive/callback' // OAuth
-// lib.Configuration.oAuthRedirectUri = `${process.env.HOST}:${process.env.PORT}/auth/pipedrive/callback` // OAuth
+// lib.Configuration.oAuthRedirectUri = '/auth/pipedrive/callback' // OAuth
+lib.Configuration.oAuthRedirectUri = `${process.env.HOST}:${process.env.PORT}/auth/pipedrive/callback` // OAuth
+console.log(lib.Configuration.oAuthRedirectUri)
 
 module.exports = async (appServer) => {
   appServer.get('/auth_user', async (req, res) => {
