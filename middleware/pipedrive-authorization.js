971e6d33639293d301530530aad11903dae99603
rogerrafael7 <rogerrafael7@gmail.com>
2020-11-03 05:33:52 -0300

    ...

--- middleware/pipedrive-authorization.js
+++ middleware/pipedrive-authorization.js
@@ -4,7 +4,7 @@ const oAuthManager = lib.OAuthManager
 lib.Configuration.apiToken = process.env.PIPEDRIVE_API_TOKEN
 lib.Configuration.oAuthClientId = process.env.PIPEDRIVE_CLIENT_ID // OAuth 2 Client ID
 lib.Configuration.oAuthClientSecret = process.env.PIPEDRIVE_CLIENT_SECRET // OAuth 2 Client Secret
-lib.Configuration.oAuthRedirectUri = `http://${process.env.HOST}:${process.env.PORT}/auth/pipedrive/callback` // OAuth
+lib.Configuration.oAuthRedirectUri = `${process.env.HOST}:${process.env.PORT}/auth/pipedrive/callback` // OAuth
 
 module.exports = async (appServer) => {
   appServer.get('/auth_user', async (req, res) => {
@@ -28,7 +28,7 @@ module.exports = async (appServer) => {
 
   return async (req, res, next) => {
     try {
-      console.log('req.session', req.session)
+      console.log('req.session', req.session.token)
       if (req.session.token !== null && req.session.token !== undefined) {
         // token is already set in the session
         // now make API calls as required
