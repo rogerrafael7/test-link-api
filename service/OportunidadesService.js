eaab5d33fc1d4e180f7bf86d93b033ef82a668f8
rogerrafael7 <rogerrafael7@gmail.com>
2020-11-04 08:44:05 -0300

    ...

--- service/OportunidadesService.js
+++ service/OportunidadesService.js
@@ -79,7 +79,9 @@ class OportunidadesService extends AbstractService {
     }
 
     const inserirNovoPedidoBling = async () => {
-      const { data: { contatos } } = await axios.get(`https://bling.com.br/Api/v2/contatos/json?apikey=${process.env.BLING_API_KEY}`)
+      const { data } = await axios.get(`https://bling.com.br/Api/v2/contatos/json?apikey=${process.env.BLING_API_KEY}`)
+      console.log(data)
+      const { contatos } = data
       const unicoContatoFornecedor = contatos[0]
       const { data: { produtos } } = await axios.get(`https://bling.com.br/Api/v2/produtos/json?apikey=${process.env.BLING_API_KEY}`)
       const unicoProduto = produtos[0]
